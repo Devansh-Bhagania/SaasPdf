6e5b56a70927c2053b98d3567ad9222d0e716b22
Devansh Bhagania <117916160+Devansh-Bhagania@users.noreply.github.com>
2023-10-30 20:29:31 +0530

    yes tried to change the navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -11,28 +11,36 @@ const Navbar = async () => {
     console.log("jfidsfkb")
     // console.log(session.user);
     return (
-        
-        <div className='w-full px-4 py-8 bg-gray-300 flex flex-row items-center gap-4'>
-            <Link href='/'>Home</Link>
-            <Link href='/Addpdf'>Add PDF</Link>
-            <Link href='/protected/dashboard'>Dashboard</Link>
+        <>
+        <div className='w-full px-4 py-8 bg-gray-300 space-between flex flex-row  gap-4'>
+            
 
+     
+
+               <Link href='/'>Home</Link>
+                <Link href='/Addpdf'>Add PDF</Link>
+                <Link href='/protected/dashboard'>Dashboard</Link>
+                
             {session && session.user?.email ? (
-                <>
+                     <>
                     <Link href='/auth/signout'>Sign out</Link>
                     <p>
                         <b>Signed in as {session.user?.email}</b>
                     </p>
-                </>
-            ) : (
-                <>
+                    </>
+                     ) : (
+                    <>
                     <Link href='/auth/signin'>Sign in</Link>
                     <Link href='/auth/signup'>Sign up</Link>
-                </>
+                    </>
             )}
+             
 
-          
+            
+
+            
         </div>
+        </>
     );
 };
 
